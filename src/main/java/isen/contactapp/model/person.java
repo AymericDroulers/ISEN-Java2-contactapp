c7c4d1aa434f65d5a58523f5e8c567afb57b6e81
Aymeric Droulers <droulers.aymeric@gmail.com>
2026-02-13 14:44:04 +0100

    feat: add person model

---
+++ src/main/java/isen/contactapp/model/person.java
@@ -0,0 +1,91 @@
+package isen.contactapp.model;
+
+
+import java.util.Date;
+
+public class person {
+    private int idPerson;
+    private String lastname;
+    private String firstname;
+    private String nickname;
+    private String phone_number;
+    private String address;
+    private String email_address;
+    private Date birth_date;
+
+
+    public person(int idPerson, String lastname, String firstname, String nickname, String phone_number, String address, String email_address, Date birth_date) {
+        this.idPerson = idPerson;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.nickname = nickname;
+        this.phone_number = phone_number;
+        this.address = address;
+        this.email_address = email_address;
+        this.birth_date = birth_date;
+    }
+
+    public int getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(int idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module isen.contactapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens isen.contactapp to javafx.fxml;
